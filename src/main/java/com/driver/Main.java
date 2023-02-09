08a3b6715b6cefa19e8906c7f7e52beb3a38a040
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2023-02-09 09:28:44 +0530

    faltu error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,6 @@
 package com.driver;
 
-class Product{
-    public int product(int x, int y){
-        return x*y;
-    }
 
-    public int product(int x, int y, int z){
-        return x*y*x;
-    }
-
-    public double product(double x, double y){
-        return x*y;
-    }
-}
 public class Main {
     public static void main(String[] args) {
         Product p = new Product();
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,15 @@
+package com.driver;
+
+class Product{
+    public int product(int x, int y){
+        return x*y;
+    }
+
+    public int product(int x, int y, int z){
+        return x*y*x;
+    }
+
+    public double product(double x, double y){
+        return x*y;
+    }
+}
