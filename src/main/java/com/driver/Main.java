184e8e81c5cfe92859ef4f6536ada90c0e631844
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2023-02-09 09:24:35 +0530

    learned polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
-public class Main {
+class Product{
+    public int product(int x, int y){
+        return x*y;
+    }
 
+    public int product(int x, int y, int z){
+        return x*y*x;
+    }
+
+    public double product(double x, double y){
+        return x*y;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans1 = p.product(5,10);
+        System.out.println((ans1));
+        int ans2 = p.product(5,10,20);
+        System.out.println(ans2);
+        double ans3 = p.product(10.00,20.00);
+        System.out.println(ans3);
+    }
 }
